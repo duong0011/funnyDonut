9bd9f3eb80e65990e8b7cd7a03c92ae8072fc5ef
sr1401 <tranngasr@gmail.com>
2022-12-23 21:44:22 +0300

    card

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -39,6 +39,8 @@
         .card-container {
             height: 100%;
             width: 100%;
+            display: flex;
+            justify-content: space-around;
         }
          .cc-font::before {
           content: '';
@@ -68,7 +70,8 @@
           right: 20px;
           width: 60px;
         }
-        .cc-font .logo svg {
+        .cc-font .logo svg,
+        .cc-back .logo svg {
           width: 100%;
           height: auto;
           fill: #fff;
@@ -78,17 +81,48 @@
           background: linear-gradient(135deg, #11998e, #38ef7d);
           border-radius: 10px;
           display: block;
-          height: 195px;
+          height: 250px;
           letter-spacing: 0.1rem;
-          margin: 60px auto;
+          margin: 0 0 30px;
           position: relative;
           text-align: left;
           text-transform: uppercase;
-          width: 325px;
-          
+          padding-left: 0;
+          padding-right: 0;
         }
 
+        .black {
+            border-top: 50px solid black;
+            margin-top: 42px;
+        }
+
+        .cc-back p {
+            float: right;
+            font-size: 12px;
+            color: white;
+            margin-top: 20px;
+            padding-right: 30px;
+        }
+
+        .cvv-input {
+            text-align: center;
+            margin-top: 33px;
+        }
        
+        .cvv-input input {
+            width: 90%;
+            height: 35px;
+            border: none;
+            border-radius: 3px;
+        }
+
+        .cc-back .logo {
+            float: right;
+            padding-right: 25px;
+            margin-top: 31px;
+            width: 85px;
+        }
+
         .chip {
           position: absolute;
           width: 60px;
@@ -1365,16 +1399,16 @@
                                 PAY
                             </p>
                             <div class="containercreditcard">
-                                <div class="card-container">
-                                    <div class="cc-font">
+                                <div class="card-container row">
+                                    <div class="cc-font col l-5">
                                         <span class="chip"></span>
                                         <span class="nubank-logo"></span>
                                         <div class="logo">
-                                          <svg version="1.1" id="visa" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                               width="47.834px" height="47.834px" viewBox="0 0 47.834 47.834" style="enable-background:new 0 0 47.834 47.834;">
-                                            <g>
-                                              <g>
-                                                <path d="M44.688,16.814h-3.004c-0.933,0-1.627,0.254-2.037,1.184l-5.773,13.074h4.083c0,0,0.666-1.758,0.817-2.143
+                                            <svg version="1.1" id="visa" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                                width="47.834px" height="47.834px" viewBox="0 0 47.834 47.834" style="enable-background:new 0 0 47.834 47.834;">
+                                                <g>
+                                                    <g>
+                                                        <path d="M44.688,16.814h-3.004c-0.933,0-1.627,0.254-2.037,1.184l-5.773,13.074h4.083c0,0,0.666-1.758,0.817-2.143
                                                          c0.447,0,4.414,0.006,4.979,0.006c0.116,0.498,0.474,2.137,0.474,2.137h3.607L44.688,16.814z M39.893,26.01
                                                          c0.32-0.819,1.549-3.987,1.549-3.987c-0.021,0.039,0.317-0.825,0.518-1.362l0.262,1.23c0,0,0.745,3.406,0.901,4.119H39.893z
                                                          M34.146,26.404c-0.028,2.963-2.684,4.875-6.771,4.875c-1.743-0.018-3.422-0.361-4.332-0.76l0.547-3.193l0.501,0.228
@@ -1385,16 +1419,42 @@
                                                          C7.795,20.842,4.691,18.099,0,16.962z M17.581,16.812l-6.123,14.239l-4.114,0.007L3.862,19.161
                                                          c2.503,1.602,4.635,4.144,5.386,5.914l0.406,1.469l3.808-9.729L17.581,16.812L17.581,16.812z M19.153,16.8h3.89L20.61,31.066
                                                          h-3.888L19.153,16.8z"/>
-                                              </g>
-                                            </g>
-                                          </svg>
+                                                    </g>
+                                                </g>
+                                            </svg>
                                         </div>
                                         <span class="cc-number">5032 9334 3764 9846</span>
                                         <span class="cc-date expedition">01/14</span>
                                         <span class="cc-date expiry">01/99</span>
                                         <span class="cc-name">Cassio Cardoso</span>
-                                      </div> <!-- /.cc -->
-                                      <div class="cc-back">1111111111111</div>
+                                    </div> 
+                                    <div class="cc-back col l-5">
+                                        <div class="black"></div>
+                                        <p>CVV</p>
+                                        <div class="cvv-input">
+                                            <input type="text" name="" id="">
+                                        </div>
+                                        <div class="logo">
+                                            <svg version="1.1" id="visa" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                                width="47.834px" height="47.834px" viewBox="0 0 47.834 47.834" style="enable-background:new 0 0 47.834 47.834;">
+                                                <g>
+                                                    <g>
+                                                        <path d="M44.688,16.814h-3.004c-0.933,0-1.627,0.254-2.037,1.184l-5.773,13.074h4.083c0,0,0.666-1.758,0.817-2.143
+                                                         c0.447,0,4.414,0.006,4.979,0.006c0.116,0.498,0.474,2.137,0.474,2.137h3.607L44.688,16.814z M39.893,26.01
+                                                         c0.32-0.819,1.549-3.987,1.549-3.987c-0.021,0.039,0.317-0.825,0.518-1.362l0.262,1.23c0,0,0.745,3.406,0.901,4.119H39.893z
+                                                         M34.146,26.404c-0.028,2.963-2.684,4.875-6.771,4.875c-1.743-0.018-3.422-0.361-4.332-0.76l0.547-3.193l0.501,0.228
+                                                         c1.277,0.532,2.104,0.747,3.661,0.747c1.117,0,2.313-0.438,2.325-1.393c0.007-0.625-0.501-1.07-2.016-1.77
+                                                         c-1.476-0.683-3.43-1.827-3.405-3.876c0.021-2.773,2.729-4.708,6.571-4.708c1.506,0,2.713,0.31,3.483,0.599l-0.526,3.092
+                                                         l-0.351-0.165c-0.716-0.288-1.638-0.566-2.91-0.546c-1.522,0-2.228,0.634-2.228,1.227c-0.008,0.668,0.824,1.108,2.184,1.77
+                                                         C33.126,23.546,34.163,24.783,34.146,26.404z M0,16.962l0.05-0.286h6.028c0.813,0.031,1.468,0.29,1.694,1.159l1.311,6.304
+                                                         C7.795,20.842,4.691,18.099,0,16.962z M17.581,16.812l-6.123,14.239l-4.114,0.007L3.862,19.161
+                                                         c2.503,1.602,4.635,4.144,5.386,5.914l0.406,1.469l3.808-9.729L17.581,16.812L17.581,16.812z M19.153,16.8h3.89L20.61,31.066
+                                                         h-3.888L19.153,16.8z"/>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <br>
